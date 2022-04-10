4e22cd6232c9e303794e20efc2a5e29f9c6da435
Pravin Yadav <pravinsy1@gmail.com>
2022-04-10 18:25:58 +0530

    update

--- index.js
+++ index.js
@@ -38,3 +38,51 @@ function lightbox_close() {
   document.getElementById('fade').style.display = 'none';
   lightBoxVideo.pause();
 }
+
+// import { createPopper } from '@popperjs/core';
+// import '../Marquee Solution/style.css';
+
+// const textbox = document.querySelector('.textbox');
+// const tooltip = document.querySelector('#tooltip');
+
+// createPopper(textbox, tooltip, {
+//   placement: 'bottom',
+//   modifiers: [
+//     {
+//       name: 'offset',
+//       options: {
+//         offset: [0, 8],
+//       },
+//     },
+//   ],
+// });
+
+// import { createPopper } from '@popperjs/core';
+// const info = document.querySelector('.info');
+// const tooltip = document.querySelector('#tooltip');
+// createPopper(info, tooltip, {
+//   placement: 'bottom',
+// });
+
+// function show() {
+//   tooltip.setAttribute('data-show', '');
+
+//   // We need to tell Popper to update the tooltip position
+//   // after we show the tooltip, otherwise it will be incorrect
+//   popperInstance.update();
+// }
+
+// function hide() {
+//   tooltip.removeAttribute('data-show');
+// }
+
+// const showEvents = ['mouseenter', 'focus'];
+// const hideEvents = ['mouseleave', 'blur'];
+
+// showEvents.forEach((event) => {
+//   button.addEventListener(event, show);
+// });
+
+// hideEvents.forEach((event) => {
+//   button.addEventListener(event, hide);
+// });
